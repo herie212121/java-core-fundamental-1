2f2be915369d5b2ee4e14166ef57bcaa7694dfe3
herie212121 <herie212121@gmail.com>
2020-10-11 08:38:24 +0700

    tambahan code di psv untuk menampilkan hasil

--- src/sesi03/PetualanganChefJaya.java
+++ src/sesi03/PetualanganChefJaya.java
@@ -10,75 +10,78 @@ package sesi03;
  * @author HERIADIE
  */
 public class PetualanganChefJaya {
-    
-  
+
     String mobil = "Chevrolet Roda 4";
     int bahanBakar = 50;
     int air = 20;
     int uang = 750000;
     String destinasi = "Sekolah";
-    
-    
+
     public static void main(String[] args) {
-      for (int x=1;x<20; x++){
-         
-      }
+        PetualanganChefJaya exercise = new PetualanganChefJaya();
+        exercise.pergiKeSekolah();
+        exercise.pergiKePasar();
+        exercise.pergiKeBengkelBan();
+        exercise.pergiKePomBensin();
+        exercise.pergiKePeternakan();
+        
     }
-    public void pergiKeSekolah (){
-       mesinBerjalan();
-       bahanBakar = 49;
-       uang = 70000;
-       air = 20;
+
+    public void pergiKeSekolah() {
+        mesinBerjalan();
+        bahanBakar = 49;
+        uang = 70000;
+        air = 20;
         System.out.println("sisa bahan bakar =" + bahanBakar);
         System.out.println("sisa uang =" + uang);
-        System.out.println("sisa air" + air );
+        System.out.println("sisa air" + air);
     }
-    
-    public void pergiKePasar (){
-       mesinBerjalan();
+
+    public void pergiKePasar() {
+        mesinBerjalan();
         bahanBakar = 20;
-       uang = 60000;
-       air = 15;
-       System.out.println("sisa bahan bakar =" + bahanBakar);
+        uang = 60000;
+        air = 15;
+        System.out.println("sisa bahan bakar =" + bahanBakar);
         System.out.println("sisa uang =" + uang);
-        System.out.println("sisa air" + air );
+        System.out.println("sisa air" + air);
     }
-    
-    public void pergiKeBengkelBan (){
-       mesinBerjalan();
+
+    public void pergiKeBengkelBan() {
+        mesinBerjalan();
         bahanBakar = 50;
-       uang = 50000;
-       air = 14;
-       System.out.println("sisa bahan bakar =" + bahanBakar);
+        uang = 50000;
+        air = 14;
+        System.out.println("sisa bahan bakar =" + bahanBakar);
         System.out.println("sisa uang =" + uang);
-        System.out.println("sisa air" + air );
+        System.out.println("sisa air" + air);
     }
-    
-    public void pergiKePomBensin (){
-       mesinBerjalan();
+
+    public void pergiKePomBensin() {
+        mesinBerjalan();
         bahanBakar = 45;
-       uang = 30000;
-       air = 13;
-       System.out.println("sisa bahan bakar =" + bahanBakar);
+        uang = 30000;
+        air = 13;
+        System.out.println("sisa bahan bakar =" + bahanBakar);
         System.out.println("sisa uang =" + uang);
-        System.out.println("sisa air" + air );
+        System.out.println("sisa air" + air);
     }
-    
-    public void pergiKePeternakan (){
-       mesinBerjalan();
+
+    public void pergiKePeternakan() {
+        mesinBerjalan();
         bahanBakar = 25;
-       uang = 25000;
-       air = 5;
-       System.out.println("sisa bahan bakar =" + bahanBakar);
+        uang = 25000;
+        air = 5;
+        System.out.println("sisa bahan bakar =" + bahanBakar);
         System.out.println("sisa uang =" + uang);
-        System.out.println("sisa air" + air );
+        System.out.println("sisa air" + air);
     }
-    public void mesinBerjalan(){
-     for (int x=1; x<8; x++) {
+
+    public void mesinBerjalan() {
+        for (int x = 1; x < 8; x++) {
             System.out.println("mobil berjalan .... brrrmmmm....");
         }
-           
-            
+
     }
-    
+
 }
